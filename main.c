2559db29c535e137054cb7970eeaf2657385b629
Deg117 <brian.gastelum@uabc.edu.mx>
2025-09-12 10:10:19 -0700

    avance

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+printf("dime tu peso\n");
+return 0;
+}
+//agregar menu: agregar libro,mostrar libros, agregar miembro
+//Funciones oir operacion
+//Memoria dianmica estructuras manejo de memoria 
